e28ff5514c1eb6106040b58dac2cd0d275f4352e
Pedro Cisternas <pedrocisternasd@gmail.com>
2021-03-23 01:14:18 -0300

    Many message per image

---
+++ src/resources/dialogues.gd
@@ -0,0 +1,14 @@
+class_name Dialogues extends GDScript
+
+const NEXT = "->"
+const PROLOGUE = [
+	"Hola hola",
+	"segundo mensaje",
+	"ahora sí siguiente imagen",
+	NEXT,
+	"Acá solo hay un mensaje largo, pasemos a la siguiente imagen",
+	NEXT,
+	"Mensaje 1",
+	"Mensaje 2",
+	"Funcionó todo"
+]
