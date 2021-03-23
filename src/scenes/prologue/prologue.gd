23b2fb7846218b98197dd5667cc0b61d944ae3ff
Pedro Cisternas <pedrocisternasd@gmail.com>
2021-03-23 01:13:56 -0300

    Many message per image

--- src/scenes/prologue/prologue.gd
+++ src/scenes/prologue/prologue.gd
@@ -2,23 +2,25 @@ class_name Prologue extends BaseNode
 
 var image_manager: ImageManager
 var message: Message
-var delay: Delay
 var images: Array = [
 	"res://assets/prologue/1.png",
 	"res://assets/prologue/2.png",
 	"res://assets/prologue/3.png",
 	"res://assets/prologue/4.png",
 ]
-var messages: Array = [
-	"weeeena wenaaaaas",
-	"este es un test de mensajes",
-	"ha funcionado!"
-]
+var DIALOGUES: GDScript
 
 func _init():
+	DIALOGUES = Load.src("resources/dialogues")
 	image_manager = Load.src("utils/image_manager/image_manager", images)
 	message = Load.src("utils/message/message")
-	delay = Load.src("utils/timer/delay", "show_next_message", 2)
+
+func _process(delta):
+	listen_start_button()
+
+func listen_start_button()-> void:
+	if Input.is_action_just_released("ui_accept"):
+		show_next_message()
 
 func start()-> void:
 	show_images()
@@ -29,18 +31,19 @@ func show_images()-> void:
 
 func show_messages():
 	add_child(message)
-	add_child(delay)
-	message.show_message(messages.pop_front())
-	delay.start()
+	message.show_message(DIALOGUES.PROLOGUE.pop_front())
 
 func show_next_message():
 	if message.is_complete:
-		var text = messages.pop_front()
-		message.show_message(text)
-		image_manager.next()
+		if DIALOGUES.PROLOGUE.empty():
+			#iniciar juego o siguiente secuencia
+			pass
+		else:
+			var text = DIALOGUES.PROLOGUE.pop_front()
+			if text == DIALOGUES.NEXT:
+				image_manager.next()
+				message.show_message(DIALOGUES.PROLOGUE.pop_front())
+			else:
+				message.show_message(text)
 	else:
 		message.complete()
-	
-	if messages.empty():
-		delay.stop()
-
