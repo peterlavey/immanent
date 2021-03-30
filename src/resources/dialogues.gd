195d89a37e66a687a20f1d1d664187270b45944c
Pedro Cisternas <pedrocisternasd@gmail.com>
2021-03-30 20:36:24 -0300

    Prologue pictures & dialogues updated

--- src/resources/dialogues.gd
+++ src/resources/dialogues.gd
@@ -2,15 +2,20 @@ class_name Dialogues extends GDScript
 
 const NEXT = "->"
 const PROLOGUE = [
-	"Ha llegado el día del juicio final, la humanidad al fin logra ver el origen de todo cara a cara",
+	"Hemos superado muchos obstaculos, luchado guerras, desarrollado tecnología y avances médicos",
+	"Todo con el fin de derrotar a la muerte y desafiar nuestra naturaleza",
+	"Cuando lo conseguimos la ciencia comenzó a dar pasos gigantes gracias a que los cientificos viven lo suficiente para ser eruditos",
+	"Nuestro segundo objetivo fue conocer el origen de todo... también lo conseguimos, pero con ello comprendimos que nuestro fin se acercaba",
+	"Desde entonces hemos búscado una manera de superar este último obstaculo, y paradojicamente la respuesta de la ciencia es que tengamos fé en este proyecto",
+	"El proyecto GENESIS",
+	NEXT,
+	"Ha llegado el día del juicio final, no existe material ni energía que nos salve esta vez",
+	"La humanidad al fin logra ver el origen de todo cara a cara",
+	"Según nuestros calculos quedan siente días",
+	NEXT,
 	"Este podría ser nuestro fin y el inicio de un nuevo ciclo",
 	"Pero aún tenemos una esperanza...",
-	"Ha llegado el momento de desafiar a la creación.",
-	"[shake rate=13 level=6]ESTA ES LA RESPUESTA DE LA HUMANIDAD![/shake]",
+	"Desafiaremos a la creación y viviremos para continuar nuestro legado.",
 	NEXT,
-	"Acá solo hay un mensaje largo, pasemos a la siguiente imagen",
-	NEXT,
-	"Mensaje 1",
-	"Mensaje 2",
-	"Funcionó todo"
+	"[shake rate=13 level=6]ESTA ES LA RESPUESTA DE LA HUMANIDAD![/shake]"
 ]
